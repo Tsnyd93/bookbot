a5462e22f2a04a8a324ac23f1e22bb781e4f3e9b
Tsnyd93 <dbmnekro817@gmail.com>
2025-07-11 18:02:47 -0500

    cleaned up organization of code

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 def main():
     import sys
+    from stats import sorted
+    from stats import get_num_words
+    from stats import lower_case
+    from stats import sorted
     if len(sys.argv) <2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
 
     book_path = sys.argv[1]
     text = get_book_text(book_path)
-    from stats import sorted
-    from stats import get_num_words
     num_words = get_num_words(text)
     print(f"Found {num_words} total words")
-    from stats import lower_case
-    low_count=lower_case(text)
-    from stats import sorted
+    lower_case_count=lower_case(text)
     sorted_dict=sorted(low_count)
     for char in sorted_dict:
         char_string= char["char"]
@@ -20,11 +20,6 @@ def main():
             print(f"{char_string}: {char['num']}")
         
             
-
-        
-    
-    
-
 def get_book_text(path):
     with open(path) as f:
         return f.read()
